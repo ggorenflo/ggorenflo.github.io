310ccb58a9e9d6bbcb2ad72978bff01a1d40eb18
Sean Fitzgerald <sfitzmill@gmail.com>
2019-03-19 01:23:24 -0500

    added uikit files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Home from './views/Home';
+import About from './views/About';
 import './App.css';
+import 'uikit/dist/css/uikit.min.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div data-uk-grid className="uk-grid uk-child-width-expand">
+          <div>
+            <Route path="/" exact component={Home} />
+            <Route path="/about/" component={About} />
+          </div>
+          <div className="uk-width-1-5">
+            <ul className="uk-nav uk-nav-primary">
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about/">About</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </Router>
     );
   }
 }
