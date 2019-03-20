6ceacf77ffa96b81857f75863fc27cc258709563
Sean Fitzgerald <sfitzmill@gmail.com>
2019-03-19 22:44:32 -0500

    added mobile friendly nav

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,24 +1,26 @@
 import React, { Component } from 'react';
-import logo from '../assets/logo.svg';
 
 export default class Home extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h1>Grace Gorenflo</h1>
+        <div className="uk-section uk-section-muted">
+          <div className="uk-container uk-grid uk-grid-medium uk-child-width-1-3@s uk-text-center" data-uk-grid>
+            <div>
+              <span uk-icon="icon: quote-right; ratio: 3"></span>
+              <p className="uk-text-lead">Researcher</p>
+            </div>
+            <div>
+              <span uk-icon="icon: pencil; ratio: 3"></span>
+              <p className="uk-text-lead">Writer</p>
+            </div>
+            <div>
+              <span uk-icon="icon: strikethrough; ratio: 3"></span>
+              <p className="uk-text-lead">Editor</p>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
---
+++ src/views/Portfolio.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Portfolio extends Component {
+  render() {
+    return (
+      <div>
+        <p className="uk-text-lead">Portfolio</p>
+      </div>
+    );
+  }
+}
