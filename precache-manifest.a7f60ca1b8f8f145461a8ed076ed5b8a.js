7baa18addbe6d1a386477fe50556b40530894e87
Sean Fitzgerald <sfitzmill@gmail.com>
2020-04-20 13:00:00 -0500

    Updates

---
+++ precache-manifest.a7f60ca1b8f8f145461a8ed076ed5b8a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24bfd4c34d6e2a7f8237f6ce7af466a0",
+    "url": "/grace/index.html"
+  },
+  {
+    "revision": "b35261cd72bc79f5ead6",
+    "url": "/grace/static/css/main.3acb12d8.chunk.css"
+  },
+  {
+    "revision": "d59c755da2b64bee0e31",
+    "url": "/grace/static/js/2.f4cb7697.chunk.js"
+  },
+  {
+    "revision": "b35261cd72bc79f5ead6",
+    "url": "/grace/static/js/main.9ec6e420.chunk.js"
+  },
+  {
+    "revision": "b227ce719e80afba55f3",
+    "url": "/grace/static/js/runtime~main.9e664ae7.js"
+  },
+  {
+    "revision": "d3a6c9ef1f1675481058ff1549bc46e0",
+    "url": "/grace/static/media/new-profile.d3a6c9ef.png"
+  },
+  {
+    "revision": "43e847d43a4abfb3c5b244a53b759949",
+    "url": "/grace/static/media/podcasts.43e847d4.csv"
+  },
+  {
+    "revision": "8a43aa6298ebf3eb837fc87be2b65bb4",
+    "url": "/grace/static/media/stories.8a43aa62.csv"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/grace/precache-manifest.937c58cd8a8698ee29ff72d05d5246ae.js"
+  "/grace/precache-manifest.a7f60ca1b8f8f145461a8ed076ed5b8a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/grace/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
